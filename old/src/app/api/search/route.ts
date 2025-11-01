b3abb88d5c26d2c750f1da442744191b41724aa5
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2025-11-01 09:30:27 +0100

    cleaned old to local dir

--- old/src/app/api/search/route.ts
+++
@@ -1,4 +0,0 @@
-import { bsrc } from "@/lib/source";
-import { createFromSource } from "fumadocs-core/search/server";
-
-export const { GET } = createFromSource(bsrc);
--- old/src/lib/project.data.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from "~/types/project.types";
-
-const Projects: Project[] = [];
-
-Projects.push(
-  new Project({
-    identifier: "",
-  }),
-);
